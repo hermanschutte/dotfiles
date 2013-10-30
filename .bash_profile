1624f33006bfdea26352ec133d9753a6048ef324
Herman Schutte <herman.schutte@gmail.com>
2013-10-30 00:00:58 -0400

    Initial commit

---
+++ .bash_profile
@@ -0,0 +1,54 @@
+source ~/git-completion.bash
+
+# Git aliases
+alias gco='git co'
+alias gci='git ci'
+alias grb='git rb'
+
+# Sublime Text 2
+alias e='subl . &'
+
+# Add `~/bin` to the `$PATH`
+export PATH="/usr/local/bin:$PATH"
+
+# Colorful bash
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+export GREP_OPTIONS='--color=auto'
+
+PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
+
+# Case-insensitive globbing (used in pathname expansion)
+shopt -s nocaseglob
+
+# Append to the Bash history file, rather than overwriting it
+shopt -s histappend
+
+# Autocorrect typos in path names when using `cd`
+shopt -s cdspell
+
+# Enable some Bash 4 features when possible:
+# * `autocd`, e.g. `**/qux` will enter `./foo/bar/baz/qux`
+# * Recursive globbing, e.g. `echo **/*.txt`
+for option in autocd globstar; do
+        shopt -s "$option" 2> /dev/null
+done
+
+# Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2 | tr ' ' '\n')" scp sftp ssh
+
+# Add tab completion for `defaults read|write NSGlobalDomain`
+# You could just use `-g` instead, but I like being explicit
+complete -W "NSGlobalDomain" defaults
+
+# Add `killall` tab completion for common apps
+complete -o "nospace" -W "Contacts Calendar Dock Finder Mail Safari iTunes SystemUIServer Terminal Twitter" killall
+
+# If possible, add tab completion for many more commands
+[ -f /etc/bash_completion ] && source /etc/bash_completion
+
+# If there is a bashrc, include it
+if [ -f ~/.bashrc ]; then
+   source ~/.bashrc
+fi
+
---
+++ .config/fish/config.fish
@@ -0,0 +1,119 @@
+if status --is-login
+	for p in /usr/bin /usr/local/bin /opt/local/bin /usr/local/mysql/bin /opt/local/lib/postgresql83/bin ~/bin ~/.config/fish/bin
+		if test -d $p
+			set PATH $p $PATH
+		end
+	end
+end
+
+
+
+set fish_greeting ""
+set -x CLICOLOR 1
+
+function parse_git_branch
+	sh -c 'git branch --no-color 2> /dev/null' | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+end
+
+function parse_git_tag
+	git describe --tags --always ^/dev/null
+end
+
+function parse_git_tag_or_branch
+	if [ (parse_git_branch) != "(no branch)" ]
+        	parse_git_branch
+	else
+        	parse_git_tag
+	end
+end
+
+
+function git_parse_ahead_of_remote
+	git status ^/dev/null | grep 'Your branch is ahead of' | sed -e 's/# Your branch is ahead of .* by \(.*\) commit.*/\1/g'
+end
+
+function parse_git_uncommited_changes
+    git status ^/dev/null | grep 'Changes' | sed -e 's/# Changes\(.*\)/\1/g'
+end
+
+function is_git
+	git status >/dev/null ^&1
+	return $status
+end
+
+function fish_prompt -d "Write out the prompt"
+	printf '%s%s@%s%s' (set_color normal) (whoami) (hostname|cut -d . -f 1) (set_color normal)
+    # printf '%s%s' (set_color normal) (whoami) (set_color normal)
+
+	# Color writeable dirs blue, read-only dirs red
+	if test -w "."
+		printf ':%s%s' (set_color blue) (prompt_pwd)
+	else
+		printf ':%s%s' (set_color red) (prompt_pwd)
+	end
+
+	# Print git tag or branch
+	if is_git
+		printf '%s:%s%s' (set_color normal) (set_color brown) (parse_git_tag_or_branch)
+
+		#set git_ahead_of_remote (git_parse_ahead_of_remote)
+		#if [ -n "$git_ahead_of_remote" -a "$git_ahead_of_remote" != "0" ]
+	    #      printf ' +%s' (git_parse_ahead_of_remote)
+        #end
+
+        set git_uncommited_changes (parse_git_uncommited_changes)
+        if [ -n "$git_uncommited_changes" -a "$git_uncommited_changes" != "0" ]
+            printf '%s*' (set_color red)
+        end
+	end
+	printf '%s$ ' (set_color normal)
+end
+
+
+bind \cr "rake"
+
+function ss -d "Run the script/server"
+	script/server
+end
+
+function sc -d "Run the Rails console"
+	script/console
+end
+
+if test -d "/opt/java"
+   set -x JAVA_HOME "/opt/java"
+end
+
+# yarrr, add /var/lib/gems/1.8/bin to path so gems installed by the retarded ubuntu rubygems package are on the path
+set CUSTOM_GEM_PATH "/var/lib/gems/1.8/bin"
+if test -d $CUSTOM_GEM_PATH
+    set -x PATH $PATH "/var/lib/gems/1.8/bin"
+end
+
+
+# Load custom settings for current hostname
+set HOST_SPECIFIC_FILE ~/.config/fish/(hostname).fish
+if test -f $HOST_SPECIFIC_FILE
+   . $HOST_SPECIFIC_FILE
+else
+   echo Creating host specific file: $HOST_SPECIFIC_FILE
+   touch $HOST_SPECIFIC_FILE
+end
+
+# Load custom settings for current user
+set USER_SPECIFIC_FILE ~/.config/fish/(whoami).fish
+if test -f $USER_SPECIFIC_FILE
+   . $USER_SPECIFIC_FILE
+else
+   echo Creating user specific file: $USER_SPECIFIC_FILE
+   touch $USER_SPECIFIC_FILE
+end
+
+# Load custom settings for current OS
+set PLATFORM_SPECIFIC_FILE ~/.config/fish/(uname -s).fish
+if test -f $PLATFORM_SPECIFIC_FILE
+   . $PLATFORM_SPECIFIC_FILE
+else
+   echo Creating platform specific file: $PLATFORM_SPECIFIC_FILE
+   touch $PLATFORM_SPECIFIC_FILE
+end
